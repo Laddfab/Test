0bbd150c9038d005a28ad8a75fcb675e064c2187
Laddfab <emerson.porras@urp.edu.pe>
2025-08-29 09:05:52 -0500

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,14 @@
+function draw() {
+  background(0); // negro
+  fill(255); 
+  noStroke();
+
+  let r = 150 + 50 * sin(frameCount * 0.05); // radio animado
+  circle(width/2, height/2, r);
+
+  fill(200);
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  text("p5.js corriendo ✅", width/2, height - 40);
+}
 
