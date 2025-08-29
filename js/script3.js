ec88772c86adb184ddc71a36708405be95895d65
Laddfab <emerson.porras@urp.edu.pe>
2025-08-29 11:50:47 -0500

    Update script3.js

--- js/script3.js
+++ js/script3.js
@@ -1,14 +1,17 @@
-// Escena mínima: si esto falla, mira la consola.
+// Escena mínima: si esto falla, mira la consola (F12)
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000);
 camera.position.z = 3;
 
 const renderer = new THREE.WebGLRenderer({ antialias:true });
 renderer.setSize(innerWidth, innerHeight);
-renderer.setClearColor(0x202124);
+renderer.setClearColor(0x202124, 1);
 document.body.appendChild(renderer.domElement);
 
-const cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshNormalMaterial());
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  new THREE.MeshNormalMaterial() // NO necesita luces
+);
 scene.add(cube);
 
 function animate(){
