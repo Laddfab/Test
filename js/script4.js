9cc21dd624ff6635ae0e55cb01b6c16109a582de
Laddfab <emerson.porras@urp.edu.pe>
2025-08-30 18:57:10 -0500

    Update script4.js

--- js/script4.js
+++ js/script4.js
@@ -12,7 +12,7 @@ const pane = document.getElementById("three-pane");
    Ej: https://.../index.html?model=assets/Test1.glb
 ========================= */
 const params = new URLSearchParams(location.search);
-const MODEL_PATH = params.get("model") || "../assets/rhino1.glb";
+const MODEL_PATH = params.get("model") || "../assets/test5.glb";
 
 /* =========================
    Escena / cámara / renderer
